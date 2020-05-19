2f5e477e1d974566aaabbe70f1ed4b348d2c5992
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-05-19 14:45:37 -0600

    iterative sorting tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,30 +2,60 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        curr_index = i
+        smallest_index = curr_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for smallest_index in range(i + 1, len(arr)):
+            if arr[smallest_index] < arr[curr_index]:
+                curr_index = smallest_index
 
         # TO-DO: swap
         # Your code here
-
+        arr[i], arr[curr_index] = arr[curr_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    n = len(arr) 
 
+    # Traverse through all array elements 
+    for i in range(n): 
 
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
-    # Your code here
+    if len(arr) == 0:
+        return arr
 
+    if maximum == -1:
+        maximum = max(arr)
+
+    buckets = [0 for i in range(maximum+1)]
+
+    for x in arr:
+        if x < 0:
+            return "Error, negative numbers disallowed"
+        buckets[x] += 1
+
+    j = 0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j +=1
+            buckets[i] -+ 1
 
     return arr
